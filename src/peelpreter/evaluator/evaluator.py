2f39928f4f7f4badeacd4579229bcf4ceefb468d
GogiBoy102 <samajdwarjeebak2968@outlook.com>
2024-05-27 14:53:37 +0530

    Fix: Added __future__ Import Preemptively
    Added a `from __future__ import annotations` preemptively to evaluator

--- src/peelpreter/evaluator/evaluator.py
+++ src/peelpreter/evaluator/evaluator.py
@@ -18,6 +18,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##############################################################################
 
+from __future__ import annotations
 from sys import setrecursionlimit
 from typing import Union
 
