6195de088db8f1902d6bfae842c1ba1d7accb026
GogiBoy102 <samajdwarjeebak2968@outlook.com>
2024-06-11 14:53:58 +0530

    fix(objectt): Added __futute__ import for 3.7 compatibility

--- src/peelpreter/objectt/objectt.py
+++ src/peelpreter/objectt/objectt.py
@@ -18,6 +18,8 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ########################################################################################
 
+from __future__ import annotations 
+
 from typing import TYPE_CHECKING, Callable
 
 from .. import astt
